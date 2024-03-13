ee30e0e3d5b104dc9055facd559d62201e2e1dfe
Stepa <Stepanovekir@yandex.ru>
2024-03-13 22:41:06 +0300

    Первый коммит для ветки controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,10 +3,19 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Duration;
+import java.time.Instant;
+
 /**
  * Film.
  */
 @Getter
 @Setter
 public class Film {
+    int id;
+    String name;
+    String description;
+    Instant releaseDate;
+    Duration duration;
+
 }
