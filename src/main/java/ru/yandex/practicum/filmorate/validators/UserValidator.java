59f8fe4ad5167eca4fb30b5934de66a873e288a2
Stepanov.k <Stepanovekir@yandex.ru>
2024-03-19 16:39:37 +0300

    Исправления по тестам №5 Update exception+format

--- src/main/java/ru/yandex/practicum/filmorate/validators/UserValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/UserValidator.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.validators;
 
 import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.model.User;
+
 @Slf4j
 public class UserValidator {
     public static boolean validateUser(User user) throws ValidationException {
