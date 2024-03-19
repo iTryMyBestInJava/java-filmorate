95125199b22c0b3fe2fc2eb9c7ebd053b87bcf48
Stepanov.k <Stepanovekir@yandex.ru>
2024-03-19 16:01:48 +0300

    Исправления по тестам №2 Unused imports

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
-import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @SpringBootTest
 class FilmorateApplicationTests {
@@ -15,5 +9,4 @@ class FilmorateApplicationTests {
 	void contextLoads() {
 
 	}
-
 }
