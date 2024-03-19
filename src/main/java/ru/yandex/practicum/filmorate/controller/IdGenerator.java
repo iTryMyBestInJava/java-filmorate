1f55660a5a06d62e044e001872de417b8453964a
Stepanov.k <Stepanovekir@yandex.ru>
2024-03-19 17:04:13 +0300

    Исправления по тестам №8 пробуем два генератора id

--- src/main/java/ru/yandex/practicum/filmorate/controller/IdGenerator.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/IdGenerator.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum.filmorate.controller;
 public class IdGenerator {
     public static Integer filmId = 0;
     public static Integer userId = 0;
+
     public static Integer generateFilmId() {
         return ++filmId;
     }
+
     public static Integer generateUserId() {
         return ++userId;
     }
