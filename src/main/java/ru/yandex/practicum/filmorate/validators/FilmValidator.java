42b0d140e28798cc8c1f4adaf7be687b648d1226
Stepanov.k <Stepanovekir@yandex.ru>
2024-03-19 16:55:52 +0300

    Исправления по тестам №7 releaseDate min date fix

--- src/main/java/ru/yandex/practicum/filmorate/validators/FilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/FilmValidator.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 @Slf4j
 public class FilmValidator {
     public static boolean validateFilm(Film film) throws ValidationException {
-        if (film.getReleaseDate().isBefore(LocalDate.parse("1985-12-28"))) {
+        if (film.getReleaseDate().isBefore(LocalDate.parse("1895-12-28"))) {
             log.warn("Валидация не пройдена, введена неверная дата");
             throw new ValidationException("Неверная дата");
         }
